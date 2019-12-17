f956bad1c1cf4b3f160ba5eb95ea4a98d17511c2
AriHildaMawaddah <34573816+AriHildaMawaddah@users.noreply.github.com>
2019-12-17 13:36:00 +0700

    Adding to Github

---
+++ Assets/script/popup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popup : MonoBehaviour {
+    public GameObject PopUp;
+    public bool aktif;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnMouseDown()
+    {
+        PopUp.SetActive(aktif);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/script/score.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class score : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PlayerPrefs.SetInt("skor", 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Text>().text = PlayerPrefs.GetInt("skor").ToString();
+	}
+}
